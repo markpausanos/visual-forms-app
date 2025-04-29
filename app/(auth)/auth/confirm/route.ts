ccb02ab9a262951884ecf9aad6c972102ec8750c
markpausanos <work.markpausanos@gmail.com>
2025-04-29 21:22:33 +0800

    feat(auth): add logging for request and search parameters in GET handler

--- app/(auth)/auth/confirm/route.ts
+++ app/(auth)/auth/confirm/route.ts
@@ -5,10 +5,16 @@ import { NextRequest, NextResponse } from 'next/server';
 // The client you created from the Server-Side Auth instructions
 
 export async function GET(request: NextRequest) {
+	console.log("NextRequest", request)
+
 	const { searchParams } = new URL(request.url);
+	console.log("searchParams", searchParams)
 	const token_hash = searchParams.get('token_hash');
 	const type = searchParams.get('type') as EmailOtpType | null;
 	const next = searchParams.get('next') ?? '/';
+
+
+
 	const redirectTo = request.nextUrl.clone();
 	redirectTo.pathname = next;
 
